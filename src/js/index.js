273a7f2417f9de3fa4b77cb0c0be452ac9d6a87d
ian10 <ian.ross5@hotmail.co.uk>
2017-03-31 15:51:00 +0100

    updating git ignore file to include node modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 window.average = function (arrayOfThings) {
   var sum = 0;
+
   for(i = 0; i < arrayOfThings.length; i++) {
-    sum += arrayOfThings[i];
+      var a = arrayOfThings[i] * 1;
+      if (a) {
+          sum += a;
+      }
   }
   return sum;
 };
